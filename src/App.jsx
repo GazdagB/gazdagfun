37f4a8ad890bfa075f246e0e4d2e8c49d63a9eb9
The Code Designer <gazdagbal@gmail.com>
2023-10-04 19:38:31 +0200

    Initialize Basic Program with Bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="container">
+      <button className="btn btn-primary">Bootstrap Button</button>
+    </div>
+    </>
+  )
+}
+
+export default App
